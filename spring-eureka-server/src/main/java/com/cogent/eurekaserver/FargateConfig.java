febfcf9445a234edc3ce76e13f375b521750a2ff
dmytrozzz <d.khaynas@gmail.com>
2020-11-20 01:05:04 +0200

    final upgrade

---
+++ spring-eureka-server/src/main/java/com/cogent/eurekaserver/FargateConfig.java
@@ -0,0 +1,48 @@
+package com.cogent.eurekaserver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.appinfo.AmazonInfo;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+@Configuration
+public class FargateConfig {
+    /**
+     * We run service in fargate so override default IP when in fargate profile
+     */
+    @SneakyThrows
+    @Bean
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils,
+                                                         @Value("${server.port}") Integer port,
+                                                         @Value("${ECS_CONTAINER_METADATA_URI_V4}") String metaUrl) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        config.setDataCenterInfo(info);
+
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(metaUrl))
+                .timeout(Duration.ofMinutes(1))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        var privateIp = new ObjectMapper()
+                .readTree(response.body())
+                .at("Networks").get(0).at("IPv4Addresses").get(0).asText();
+        config.setIpAddress(privateIp);
+        config.setNonSecurePort(port);
+
+        return config;
+    }
+}
